947e9c3b9810c280ead5b27f739a2bb6deed3a16
Carl <carl.rippon@googlemail.com>
2025-01-27 18:32:03 +0000

    ch7 split

--- chapter11/start/src/App.tsx
+++ chapter11/start/src/App.tsx
@@ -1,41 +1,35 @@
-import { useState } from "react";
-import { Checklist, IdValue } from "./Checklist";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [checkedId, setCheckedId] = useState<IdValue | null>(null);
-  function handleCheckedIdsChange(newCheckedIds: IdValue[]) {
-    const newCheckedIdArr = newCheckedIds.filter((id) => id !== checkedId);
-    if (newCheckedIdArr.length === 1) {
-      setCheckedId(newCheckedIdArr[0]);
-    } else {
-      setCheckedId(null);
-    }
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Checklist
-        data={[
-          { id: 1, name: "Lucy", role: "Manager" },
-          { id: 2, name: "Bob", role: "Developer" },
-          { id: 3, name: "Bill", role: "Developer" },
-          { id: 4, name: "Tara", role: "Developer" },
-          { id: 5, name: "Sara", role: "UX" },
-          { id: 6, name: "Derik", role: "QA" },
-        ]}
-        id="id"
-        primary="name"
-        secondary="role"
-        style={{
-          width: "300px",
-          maxHeight: "380px",
-          overflowY: "auto",
-        }}
-        checkedIds={checkedId === null ? [] : [checkedId]}
-        onCheckedIdsChange={handleCheckedIdsChange}
-      />
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
